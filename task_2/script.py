613070121401245345058a23192d34939d5e02e0
Andrey <kanafjev@gmail.com>
2019-05-28 00:45:17 +0300

    добавил возможность потестировать отправку, запустив test_serv.py. добавил проверку изображения на валидность (imghdr), добавил возможност ввода параметров из командной строки

--- task_2/script.py
+++ task_2/script.py
@@ -1,11 +1,32 @@
 import requests
 import os
+import imghdr  # для проверки валидности изображения
+import argparse
 
-DIR = '/home/andrey/Prog/vision/task_2/send_img/' + input()
-URL = 'http://localhost:5000/images'
+parser = argparse.ArgumentParser(description='Lets find dir')
+parser.add_argument('path', metavar='DIR', type=str,
+                    help='put path to images here')
+parser.add_argument('url', metavar='URL', type=str,
+                    help='put url to upload')
+args = parser.parse_args()
+print(args.path, args.url)
+DIR = args.path
+URL = args.url
+# DIR = '/home/andrey/Prog/vision/task_2/send_img/' + input()
+# здесь должна быть проверка директории и соответственно вывод exceptiona
+# URL = 'http://localhost:5000/image'
 
 for item in os.listdir(DIR):
-    if '.png' in item:
-        files = {'img': open(DIR+item, 'rb')}
-        r = requests.post(URL, files=files)
-print(r)
+    if imghdr.what(DIR + item) is not None:
+        print(imghdr.what(DIR + item))
+        with open(DIR + item, 'rb') as file:
+            files = {'img': file}
+            r = requests.post(URL, files=files)
+            print(r)
+            print('uploaded file:', file.name)  # если есть параметр
+print('all images uploaded')
+
+'''
+инпут сделать из командной строки python script.py -DIR- -URL- 
+многопоточность?
+'''
