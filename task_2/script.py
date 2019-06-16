b622babfa1c277ae21ae330d9e00171b9aeef6bb
Andrey <kanafjev@gmail.com>
2019-06-16 22:11:52 +0300

    try-except заменен на проверку с помощью модуля os

--- task_2/script.py
+++ task_2/script.py
@@ -1,6 +1,6 @@
 from urllib import request
 import os
-import imghdr  # для проверки валидности изображения
+import imghdr
 import argparse
 import base64
 
@@ -9,36 +9,28 @@ parser.add_argument('path', metavar='DIR', type=str,
                     help='put path to images here')
 args = parser.parse_args()
 DIR = args.path
-# DIR = '/home/andrey/Prog/vision/task_2/send_img/' + input()
-# здесь должна быть проверка директории и соответственно вывод exceptiona
 URL = 'http://localhost:5000/images'
-try:
-    files = os.listdir(DIR)
-    print(files)
-except OSError as err:
-    files = []
-    print(err.args[1])
-for item in files:
-    print(type(item))
-    item_path = DIR + '/' + item  # ma be propriate ddelimeter
-    img_type = imghdr.what(item_path)
-    if img_type is not None:
-        print(imghdr.what(item_path))
-        with open(item_path, 'rb') as file:
-            #print(type(file))
-            img = file.read()
-            #encoded_img = img.encode('base64')
-            encoded_img = base64.b64encode(img)
-            print(type(encoded_img))
-        #data = {'img': encoded_img}
-        req = request.Request(url=URL, data=encoded_img, method='POST')
-        req.add_header('Content-Type', 'image/{}'.format(img_type))
-        resp = request.urlopen(req)
-        print(resp)
-        print('uploaded file:', file.name)  # если есть параметр
-print('all images uploaded')
 
-'''
-инпут сделать из командной строки python script.py -DIR- -URL- 
-многопоточность?
-'''
+
+def upload_image(dir_path, url):
+    if os.path.exists(dir_path) and os.path.isdir(dir_path):
+        files = os.listdir(dir_path)
+        for item in files:
+            item_path = os.path.join(os.path.abspath(DIR), item)
+            img_type = imghdr.what(item_path)  # проверяет валидность изображения по первым двум байтам
+            if img_type is not None:
+                with open(item_path, 'rb') as file:
+                    img = file.read()
+                    encoded_img = base64.b64encode(img)  # кодирует изображения в base64
+                req = request.Request(url=url, data=encoded_img, method='POST')
+                req.add_header('Content-Type', 'image/{}'.format(img_type))
+                resp = request.urlopen(req)
+                print(resp.status)
+                print('uploaded file:', file.name)  # если есть параметр
+        return 'all images uploaded'
+    else:
+        print("not a dir or dir doesn't exists")
+        return
+
+
+upload_image(DIR, URL)
--- task_2/test_serv.py
+++ task_2/test_serv.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from flask import request
-from flask import json
-import time
 import base64
 import random
 
@@ -10,11 +8,8 @@ app = Flask(__name__)
 
 @app.route('/images', methods=['POST'])
 def image_upload():
-    #x = request.data['img'].decode()
     x = base64.b64decode(request.get_data())
     ext = request.content_type
-    print(type(x))  # base64.decodestring(request.get_data())
-    #img = open('test.jpg', 'xb')
     img = open('test{}.{}'.format(random.randrange(1, 199), ext[-3:]), 'xb')
     img.write(x)
     img.close()
